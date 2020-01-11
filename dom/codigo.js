3742e5947c42ac255c96ac88412f9fc73de955ea
Elvis Geovanny <elvis.batzibal@hotmail.es>
2020-01-11 07:22:35 -0600

    control javascript

---
+++ dom/codigo.js
@@ -0,0 +1,9 @@
+alert("Si funciona");
+ //window es un objeto por defecto del navegador
+    //location es un atributo/propiedad de tipo objet 
+    var ruta= window.location;
+    console.log(ruta);
+    console.log(navigator);
+    console.log(window);
+    console.log(document);
+    document.write("El documento está en :" + ruta);
